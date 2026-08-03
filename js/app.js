14484f7083fecd21a0aab8e3ebdbac7e8fa73067
Nahuel <anahuelvega@gmail.com>
2026-08-03 19:26:54 -0300

    Add files via upload
    Los Archivos subidos correctamente

---
+++ js/app.js
@@ -0,0 +1,246 @@
+/*==================================================
+                    APP.JS
+==================================================*/
+
+
+const App={
+
+
+    tipoEntrega:null,
+
+
+    sucursal:null,
+
+
+    cliente:{
+
+
+        nombre:"",
+
+        telefono:"",
+
+        direccion:"",
+
+        entreCalles:""
+
+
+    },
+
+
+    productoActual:null,
+
+
+    carrito:[]
+
+
+};
+
+
+
+
+document.addEventListener(
+
+"DOMContentLoaded",
+
+()=>{
+
+
+    iniciarEventos();
+
+
+});
+
+
+
+
+
+
+function iniciarEventos(){
+
+
+
+const delivery=
+
+document.getElementById("btn-delivery");
+
+
+
+const retiro=
+
+document.getElementById("btn-retiro");
+
+
+
+
+if(delivery){
+
+
+delivery.onclick=()=>{
+
+
+    App.tipoEntrega="DELIVERY";
+
+
+    obtenerUbicacionCliente();
+
+
+};
+
+
+}
+
+
+
+
+if(retiro){
+
+
+retiro.onclick=()=>{
+
+
+    App.tipoEntrega="RETIRO";
+
+
+    cargarSucursales();
+
+
+    cambiarPantalla("sucursales");
+
+
+};
+
+
+}
+
+
+
+}
+
+
+
+
+
+/*==================================================
+            SELECCION SUCURSAL
+==================================================*/
+
+
+function cargarSucursales(){
+
+
+const contenedor=
+
+document.getElementById(
+
+"lista-sucursales"
+
+);
+
+
+
+if(!contenedor)return;
+
+
+
+contenedor.innerHTML="";
+
+
+
+obtenerSucursales()
+
+.forEach(sucursal=>{
+
+
+const boton=document.createElement("button");
+
+
+
+boton.className="btn-opcion";
+
+
+
+boton.innerHTML=
+
+sucursal.nombre;
+
+
+
+boton.onclick=()=>{
+
+
+App.sucursal=sucursal;
+
+
+irProductos();
+
+
+};
+
+
+
+contenedor.appendChild(boton);
+
+
+
+});
+
+
+}
+
+
+
+
+
+/*==================================================
+            PRODUCTO
+==================================================*/
+
+function seleccionarProducto(id){
+
+
+App.productoActual=
+
+obtenerProducto(id);
+
+
+
+cambiarPantalla("sabores");
+
+
+}
+
+
+
+
+
+/*==================================================
+            CARRITO
+==================================================*/
+
+
+function agregarProducto(producto){
+
+
+App.carrito.push(producto);
+
+
+
+actualizarVistaCarrito();
+
+
+
+}
+
+
+
+
+
+window.App=App;
+
+window.seleccionarProducto=
+
+seleccionarProducto;
+
+window.agregarProducto=
+
+agregarProducto;
